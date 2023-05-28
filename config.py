85414545be0de71bb9c1692cc6ef1bc24aeb0244
kimjikoin <130832705+kimjikoin@users.noreply.github.com>
2023-05-28 19:05:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001513990946")
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "『KIMJIKOIN-MUSIC』")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "KIMJIKOIN-MUSIC")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6181817811").split()))
 
