dc622a2480d65a14653c9cfa319d7dafed80910a
kimjikoin <130832705+kimjikoin@users.noreply.github.com>
2023-05-28 19:32:54 +0530

    Update bot.py

--- TeamAgora/core/bot.py
+++ TeamAgora/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 
 
-class AnonXBot(Client):
+class Xdmusic(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
